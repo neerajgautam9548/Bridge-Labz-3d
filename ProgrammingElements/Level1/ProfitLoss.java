e6111943e4d8b2b29eaf38d1413125eb7505bdd5
neeraj gautam <neerajgautam@gmail.com>
2025-08-28 17:34:29 +0530

    Initial commmit

---
+++ ProgrammingElements/Level1/ProfitLoss.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class ProfitLoss {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Cost Price: ");
+        double cp = sc.nextDouble();
+
+        System.out.print("Enter Selling Price: ");
+        double sp = sc.nextDouble();
+
+        double profit = sp - cp;
+        double profitPercent = (profit / cp) * 100;
+
+        System.out.println("The Cost Price is INR " + cp + " and Selling Price is INR " + sp +
+                "\nThe Profit is INR " + profit + " and the Profit Percentage is " + profitPercent);
+        sc.close();
+    }
+}
---
+++ ProgrammingElements/Level2/BasicCalculator.java
@@ -0,0 +1,5 @@
+package ProgrammingElements.Level2;
+
+public class BasicCalculator {
+    
+}
