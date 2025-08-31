2e83328e9a90ee687e25fa047316331bd602fb42
nishant440 <your-gautamnishant119.com>
2025-08-31 14:26:17 +0530

    Updated DayOfWeek.java

--- ControlFlows/Level3/DayOfWeek.java
+++ ControlFlows/Level3/DayOfWeek.java
@@ -3,8 +3,8 @@ import java.util.*;
 public class DayOfWeek {
     public static void main(String[] args) {
         int m = Integer.parseInt(args[0]); // month
-        int d = Integer.parseInt(args); // day
-        int y = Integer.parseInt(args); // year
+        int d = Integer.parseInt(args[1]); // day
+        int y = Integer.parseInt(args[2]); // year
 
         int y0 = y - (14 - m) / 12;
         int x = y0 + y0/4 - y0/100 + y0/400;
