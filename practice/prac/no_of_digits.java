c4b5c62f503384f1aebc31b7d37ea142669fc637
neerajgautam9548 <neerajgautam9548@gmail.com>
2025-08-31 14:40:43 +0530

    Save work before deleting prac folder

---
+++ practice/prac/no_of_digits.java
@@ -0,0 +1,10 @@
+package practice.prac;
+import java.util.Scanner;
+
+public class no_of_digits {
+    public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		System.out.println((int)Math.floor(Math.log10(x))+1);
+	}
+}
